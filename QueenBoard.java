acb75f6c928d7b2da9351a3e6725f75511741db4
bstringham <bstringham@stuy.edu>
2019-02-07 09:37:08 -0500

    Started on toString

--- QueenBoard.java
+++ QueenBoard.java
@@ -6,7 +6,11 @@ public class QueenBoard {
 	}
 
 	public String toString() {
-
+		String last = "";
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				if (board[j][i] > 0) {
+					
 	}
 	public boolean solve() {
 
