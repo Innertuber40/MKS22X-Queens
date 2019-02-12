29ad1283c8c1b6a5efc1fd5101069dd60050adf4
Innertuber <bstringham@stuy.edu>
2019-02-12 00:37:20 -0500

    Started with the necessary helper functions for solve

--- QueenBoard.java
+++ QueenBoard.java
@@ -28,6 +28,7 @@ public class QueenBoard {
 					
 
 	public boolean solve() {
+		boolean returns = false;
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
 				if (board[i][j] != 0) {
@@ -35,8 +36,40 @@ public class QueenBoard {
 				}
 			}
 		}
-		return true;
+		for (int j = 0; j < board.length; j++) {
+			returns = returns || solver(0, j);
+		}
+		return returns;
 	}
+	public void add(int i, int j) {
+		board[i][j] = -1;
+	}
+	public void undo(int i, int j) {
+		if (board[i][j] == -1) {
+			board[i][j] = 0;
+		} else {
+			board[i][j] -= 1;
+		}
+	}
+	public boolean solver(int column, int row) {
+		if (column == board.length) {
+			return true;
+		}
+		if (board[column][row] == 0) {
+			add(column, row);
+			for (int i = column + 1; i < board.length; i++) {
+				for (int j = row - 1; j > 0; j--) {
+					board[i][j] = board[i][j] + 1;
+				}
+				board[i][row] = board[i][row] + 1;
+				for (int j = row + 1; j < board.length; j++) {
+					board[i][j] = board[i][j] + 1;
+				}
+			}
+			undo(column, row);
+		}
+	}
+
 	public int countSolutions() {
 		int count = 0;
 		return count;
